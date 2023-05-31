38145dd8e37b4e4c99dc2a0643a9bf0db903c6dd
PrekshaVora <vora.preksha2001@gmail.com>
2023-05-31 20:15:57 +0530

    remove unnecessary comments

--- pomtest/addToCart.test.ts
+++ pomtest/addToCart.test.ts
@@ -1,12 +1,8 @@
 import { expect, test } from "../base/pomFixture"
 import * as data from "../test-Data/addtocart-test-data.json"
 
-// const email = "vora.preksha1@gmail.com"
-// const password = "Preksha@10"
-
 test.describe("page object test demo", async () => {
     test("Register test", async ({ page, baseURL, registerPage }) => {
-        // const register = new RegisterPage(page);
         await page.goto(`${baseURL}route=account/register`);
         await registerPage.enterFirstName(data.firstname);
         await registerPage.enterLastName(data.lastname)
@@ -20,7 +16,6 @@ test.describe("page object test demo", async () => {
     })
 
     test("Login Test", async ({ page, baseURL, loginPage }) => {
-        //const login = new LoginPage(page);
         await page.goto(`${baseURL}route=account/login`);
         await loginPage.enterEmail(data.email);
         await loginPage.enterPassword(data.password);
@@ -30,9 +25,6 @@ test.describe("page object test demo", async () => {
     })
 
     test("Add To Cart", async ({ page, baseURL, loginPage, homePage, megaMenu }) => {
-        // const login = new LoginPage(page);
-        // const homePage = new HomePage(page);
-        // const megaPage = new MegaMenu(page);
         await page.goto(`${baseURL}route=account/login`)
         await loginPage.login(data.email, data.password)
         await homePage.hoverOnMegaMenu();
